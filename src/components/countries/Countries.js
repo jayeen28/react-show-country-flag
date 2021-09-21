65b971f63eb45896cfdb4fa1f977c2b776a30cd6
MD. Jayeen Bin Alam <jayeen6783@gmail.com>
2021-09-21 19:16:35 +0600

    design changed footer added

--- src/components/countries/Countries.js
+++ src/components/countries/Countries.js
@@ -11,7 +11,7 @@ const Countries = () => {
     }, [])
     return (
         <div>
-            <h1>Hello from {countries.length}</h1>
+            <h1>{countries.length} countries data loaded</h1>
             <div className="countries-container">
                 {
                     countries.map(country => <Country country={country} key={country.alpha2Code}></Country>)
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Footer.css'
+const Footer = () => {
+    return (
+        <footer>
+            <p className="footer-text">Developed by <a href="https://jayeenbinalam.com">Jayeen</a></p>
+        </footer>
+    );
+};
+
+export default Footer;
