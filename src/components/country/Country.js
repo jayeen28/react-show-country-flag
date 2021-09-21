041a2772dba9dd81c3e23d1a0e3906091b1b98d5
MD. Jayeen Bin Alam <jayeen6783@gmail.com>
2021-09-21 18:12:39 +0600

    flag showing system has made successfully

--- src/components/country/Country.js
+++ src/components/country/Country.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import './Country.css'
 const Country = (props) => {
-    const { name, flag } = props.country;
+    const { name, capital, flag, numericCode } = props.country;
     return (
         <div className="country">
-            <h1>This is {name}</h1>
-            <img src={flag} alt="flag" className="flag" />
+            <div className="country-left">
+                <h2>Name: {name}</h2>
+                <h3>Capital: {capital}</h3>
+                <button onClick={() => showFlag(flag, 'flag' + numericCode)} className="show-flag-btn">Show Flag</button>
+            </div>
+            <div className="country-right">
+                <img alt="" className="flag" id={'flag' + numericCode} />
+            </div>
         </div>
     );
 };
+const showFlag = (flag, flagId) => {
+    const targetFlagElement = document.getElementById(flagId);
+    targetFlagElement.setAttribute('src', flag);
+}
 
 export default Country;
